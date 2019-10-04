63dd28a02c7c5f0d4d52cd02c022c51a4db921bf
Evan Cleary <evanjcleary@gmail.com>
2019-10-03 23:08:22 -0400

    Updates

---
+++ precache-manifest.d8e80dee9c0fd73d0510dd53e95ee44f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf19970f314e360968d07a2666128ba",
+    "url": "/MemoryReact/index.html"
+  },
+  {
+    "revision": "403da0b4775ff5f1c65e",
+    "url": "/MemoryReact/static/css/main.a3922ca0.chunk.css"
+  },
+  {
+    "revision": "e3f87bbf0cc548d6e94b",
+    "url": "/MemoryReact/static/js/2.20cd40a9.chunk.js"
+  },
+  {
+    "revision": "403da0b4775ff5f1c65e",
+    "url": "/MemoryReact/static/js/main.302265bd.chunk.js"
+  },
+  {
+    "revision": "eab66c7083478442eabf",
+    "url": "/MemoryReact/static/js/runtime-main.fa376dd8.js"
+  }
+]);
